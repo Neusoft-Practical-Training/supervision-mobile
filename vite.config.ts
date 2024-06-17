01a88fc100c69076a3d4b025e5bff30e6044ba36
Aftermath <From_JingWANG@outlook.com>
2024-06-17 14:07:44 +0800

    chore: install plug: @vant/auto-import-resolver unplugin-vue-components unplugin-auto-import and so on

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +14,12 @@ export default defineConfig({
     vue(),
     vueJsx(),
     VueDevTools(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
