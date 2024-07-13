6758140c970394bc742458b5483f5307c3a62cf0
Aftermath <From_JingWANG@outlook.com>
2024-07-13 12:53:04 +0800

    refactor: Extracting common utility functions: getTextColor.

--- src/util/index.ts
+++ src/util/index.ts
@@ -144,3 +144,14 @@ export const determineSPMPollutionLevel = (spmValue: number) => {
     return 7;
   }
 }
+
+// 根据背景颜色计算文本颜色，确保对比度足够
+export const getTextColor = (bgColor: string) => {
+  const color = bgColor.substring(1); // remove #
+  const rgb = parseInt(color, 16); // convert rrggbb to decimal
+  const r = (rgb >> 16) & 0xff; // extract red
+  const g = (rgb >> 8) & 0xff; // extract green
+  const b = (rgb >> 0) & 0xff; // extract blue
+  const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+  return luma > 128 ? "#000" : "#fff"; // black text if background is light, white if dark
+};
