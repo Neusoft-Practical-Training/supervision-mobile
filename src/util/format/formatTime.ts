7077b8df4806742da3f575834130a34ed29626c2
Aftermath <From_JingWANG@outlook.com>
2024-07-17 01:06:41 +0800

    refactor: refactor utils

---
+++ src/util/format/formatTime.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;  // getMonth() 返回的是 0-11，所以需要加1
+  const day = date.getDate();
+
+  // 使用模板字符串来格式化，确保月份和日期为两位数
+  return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+}
